d3212af834a04f2b5893dc0b0fbb7ba830d3a468
San2809 <53544186+San2809@users.noreply.github.com>
2019-09-12 11:34:05 -0400

    Update lognest_substr_palindrome.py

--- MEDIUM/lognest_substr_palindrome.py
+++ MEDIUM/lognest_substr_palindrome.py
@@ -1,18 +1,23 @@
-from collections import defaultdict
 class Solution(object):
+    def helper(self,s,l,r):
+        while l>=0 and r<len(s):
+            if s[l] !=s[r]:
+                break
+            l -=1
+            r +=1
+        return [l+1,r]
     def longestPalindrome(self, s):
         """
         :type s: str
         :rtype: str
         """
-        maxlen, maxp,l, dic = 0, "", len(s), defaultdict(list)
+        curr = [0,1]
         
-        for i in range(l):
-            dic[s[i]].append(i)
-            for j in dic[s[i][::-1]]:
-                if s[j:i+1] == s[j:i+1][::-1]:
-                    if len(s[j:i+1])>maxlen:
-                        maxlen = len(s[j:i+1])
-                        maxp = s[j:i+1]
-                        break
-        return maxp
+        for i in range(len(s)):
+            even = self.helper(s,i-1,i)
+            odd = self.helper(s,i-1,i+1)
+            
+            longest = max(even,odd, key=lambda X: X[1]-X[0])
+            curr = max(curr,longest, key =lambda X:X[1]-X[0])
+            
+        return s[curr[0]:curr[1]]
