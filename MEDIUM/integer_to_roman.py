99e2ffd94ba19bb0967dd0888756973ad6fa0c52
San2809 <53544186+San2809@users.noreply.github.com>
2019-09-13 18:10:26 -0400

    Update integer_to_roman.py

--- MEDIUM/integer_to_roman.py
+++ MEDIUM/integer_to_roman.py
@@ -4,16 +4,14 @@ class Solution(object):
         :type num: int
         :rtype: str
         """
+        
         values =  [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
         letters = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
-        
-        res = ""
-        
+        res =""
         i =0
-        
-        while(num>0):
+        while num>0:
             while values[i]>num:
-                i +=1
+                i+=1
             res +=letters[i]
-            num -= values[i]
+            num -=values[i]
         return res
