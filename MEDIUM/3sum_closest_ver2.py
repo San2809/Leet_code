db5ed21382e52b7d7b4d4d2adc7d7c755028c2da
San2809 <53544186+San2809@users.noreply.github.com>
2019-09-14 16:54:37 -0400

    Create 3sum_closest_ver2.py

---
+++ MEDIUM/3sum_closest_ver2.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        nums.sort()
+        res = sum(nums[:3])
+        dif = abs(res-target)
+        for i in range(len(nums)):
+            l, r = i+1, len(nums)-1
+            
+            while l<r:
+                ans = nums[l]+nums[i]+nums[r]
+                cur = abs(ans-target)
+                
+                if cur<dif:
+                    dif = cur
+                    res =ans
+                if ans<target:
+                    l +=1
+                elif ans>target:
+                    r -=1
+                
+                else:
+                    return res
+        return res
